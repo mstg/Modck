4450801662539a700c2a8d04f8450ad4abb4e988
mstg <mustiigezen@gmail.com>
2015-07-22 02:18:32 +0200

    Fix size issues when dock is not on bottom of screen

--- Modck/ModckFloor.m
+++ Modck/ModckFloor.m
@@ -31,22 +31,28 @@
 ZKSwizzleInterface(_Modck_ECMaterialLayer, ECMaterialLayer, CALayer);
 @implementation _Modck_ECMaterialLayer
 - (void)setBounds:(CGRect)arg1 {
+	NSUInteger _material = ZKHookIvar(self, NSUInteger, "_material");
+	if (_material == 5){
+		ZKOrig(void, arg1);
+		if ([[[Preferences sharedInstance] objectForKey:@"modck_labelBG"] integerValue] == 1) {
+			float red = [[[Preferences sharedInstance] objectForKey:@"modck_labelBGR"] floatValue];
+			float green = [[[Preferences sharedInstance] objectForKey:@"modck_labelBGG"] floatValue];
+			float blue = [[[Preferences sharedInstance] objectForKey:@"modck_labelBGB"] floatValue];
+			
+			NSColor *goodColor = [NSColor colorWithRed:red/255.0 green:green/255 blue:blue/255.0 alpha:1.0];
+			CALayer *tintLayer = ZKHookIvar(self, CALayer *, "_backdropLayer");
+			[tintLayer setBackgroundColor:[goodColor CGColor]];
+		}
+	}
+	
 	if ([[[Preferences sharedInstance] objectForKey:@"modck_fullWidth"] integerValue] == 1) {
 		NSString *groupName = ZKHookIvar(self, NSString *, "_groupName");
-		NSUInteger _material = ZKHookIvar(self, NSUInteger, "_material");
 		
 		if ([groupName isEqualToString:@"background"]) {
-			ZKOrig(void, CGRectMake(arg1.origin.x, arg1.origin.y, [[NSScreen mainScreen] frame].size.width, arg1.size.height));
-		} else if (_material == 5){
-			ZKOrig(void, arg1);
-			if ([[[Preferences sharedInstance] objectForKey:@"modck_labelBG"] integerValue] == 1) {
-				float red = [[[Preferences sharedInstance] objectForKey:@"modck_labelBGR"] floatValue];
-				float green = [[[Preferences sharedInstance] objectForKey:@"modck_labelBGG"] floatValue];
-				float blue = [[[Preferences sharedInstance] objectForKey:@"modck_labelBGB"] floatValue];
-				
-				NSColor *goodColor = [NSColor colorWithRed:red/255.0 green:green/255 blue:blue/255.0 alpha:1.0];
-				CALayer *tintLayer = ZKHookIvar(self, CALayer *, "_backdropLayer");
-				[tintLayer setBackgroundColor:[goodColor CGColor]];
+			if (arg1.size.height < arg1.size.width) {
+				ZKOrig(void, CGRectMake(arg1.origin.x, arg1.origin.y, [[NSScreen mainScreen] frame].size.width, arg1.size.height));
+			} else {
+				ZKOrig(void, CGRectMake(arg1.origin.x, arg1.origin.y, arg1.size.width, [[NSScreen mainScreen] frame].size.height));
 			}
 		} else {
 			ZKOrig(void, arg1);
@@ -74,7 +80,7 @@ ZKSwizzleInterface(_Modck_DOCKFloorLayer, DOCKFloorLayer, CALayer);
 		float red = [[[Preferences sharedInstance] objectForKey:@"modck_dockBGR"] floatValue];
 		float green = [[[Preferences sharedInstance] objectForKey:@"modck_dockBGG"] floatValue];
 		float blue = [[[Preferences sharedInstance] objectForKey:@"modck_dockBGB"] floatValue];
-	
+		
 		NSColor *goodColor = [NSColor colorWithRed:red/255.0 green:green/255 blue:blue/255.0 alpha:1.0];
 		[this setBackgroundColor:[goodColor CGColor]];
 		
